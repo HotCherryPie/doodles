e2bfb9a49dc75d5de96b47a5e6c561bf1a8fc077
John Doe <11800728+HotCherryPie@users.noreply.github.com>
2026-07-02 05:23:19 +0300

    wip

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import vue from '@vitejs/plugin-vue';
 import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig((config) => ({
+  base: config.command === 'build' ? '/doodles' : '/',
   plugins: [vue()],
-});
+}));
