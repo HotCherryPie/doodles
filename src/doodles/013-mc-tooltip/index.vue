31362a0617d898a13f2d235067821e6386b4e19c
John Doe <11800728+HotCherryPie@users.noreply.github.com>
2026-07-19 15:30:43 +0300

    wip

---
+++ src/doodles/013-mc-tooltip/index.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import { Bento } from '../../components';
+
+import Text from './text.vue';
+</script>
+
+<template>
+  <Bento.Cell :w="5" :h="3" cover>
+    <div :class="$style.it">
+      <div :class="$style.frame" />
+      <Text color="magenta" fancy>Bow of flame</Text>
+    </div>
+  </Bento.Cell>
+</template>
+
+<style lang="css" module>
+.it {
+  position: relative;
+  font-family: Minecraft;
+  background-color: #120211;
+  corner-shape: notch;
+  border-radius: 4px;
+  outline: 2px solid #48149a;
+  outline-offset: -4px;
+  height: 100%;
+  padding: 12px 16px;
+}
+
+.frame {
+  position: absolute;
+  image-rendering: pixelated;
+  pointer-events: none;
+  background-image: url('./frames/magenta.png');
+  background-size: contain;
+  background-repeat: no-repeat;
+  inset-block-end: 0;
+  inset-block-start: -50px;
+  background-position-y: -2px;
+  inset-inline: -9px;
+  filter: hue-rotate(-40deg);
+}
+</style>
