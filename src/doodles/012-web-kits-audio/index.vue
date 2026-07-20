aa8edc1ffeaf871ff8bee4f7d45741c634a2cd66
Oscar Fall <11800728+HotCherryPie@users.noreply.github.com>
2026-07-20 14:00:20 +0300

    wip

--- src/doodles/012-web-kits-audio/index.vue
+++ src/doodles/012-web-kits-audio/index.vue
@@ -1,26 +1,29 @@
 <script setup lang="ts">
 import { Bento } from '../../components';
 
-import { sounds } from './sounds';
+import { packs } from './sounds';
 </script>
 
 <template>
-  <Bento.Cell :w="5" :h="2">
+  <Bento.Cell :w="6" :h="3">
     <div :class="$style.root">
-      <button
-        v-for="(sound, i) of sounds"
-        :key="i"
-        type="button"
-        data-silent
-        :class="$style.btn"
-        @click="sound()"
-      >
-        <svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
-          <path
-            d="M4.74514 3.06414C4.41183 2.87665 4 3.11751 4 3.49993V12.5002C4 12.8826 4.41182 13.1235 4.74512 12.936L12.7454 8.43601C13.0852 8.24486 13.0852 7.75559 12.7454 7.56443L4.74514 3.06414ZM3 3.49993C3 2.35268 4.2355 1.63011 5.23541 2.19257L13.2357 6.69286C14.2551 7.26633 14.2551 8.73415 13.2356 9.30759L5.23537 13.8076C4.23546 14.37 3 13.6474 3 12.5002V3.49993Z"
-          />
-        </svg>
-      </button>
+      <template v-for="(pack, name) of packs" :key="name">
+        <button
+          v-for="(sound, i) of pack"
+          :key="i"
+          :title="i"
+          type="button"
+          data-silent
+          :class="$style.btn"
+          @click="sound()"
+        >
+          <svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
+            <path
+              d="M4.74514 3.06414C4.41183 2.87665 4 3.11751 4 3.49993V12.5002C4 12.8826 4.41182 13.1235 4.74512 12.936L12.7454 8.43601C13.0852 8.24486 13.0852 7.75559 12.7454 7.56443L4.74514 3.06414ZM3 3.49993C3 2.35268 4.2355 1.63011 5.23541 2.19257L13.2357 6.69286C14.2551 7.26633 14.2551 8.73415 13.2356 9.30759L5.23537 13.8076C4.23546 14.37 3 13.6474 3 12.5002V3.49993Z"
+            />
+          </svg>
+        </button>
+      </template>
     </div>
   </Bento.Cell>
 </template>
