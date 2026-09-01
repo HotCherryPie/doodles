1fede5dfa994534dc4dc1f6d1eced23fdcf1a9ea
John Doe <11800728+HotCherryPie@users.noreply.github.com>
2026-09-02 00:05:35 +0300

    wip

--- src/app.vue
+++ src/app.vue
@@ -10,8 +10,15 @@ import { RouterView } from 'vue-router';
 <style lang="css">
 @import url(./styles/fonts.css);
 
+/* TODO: move to reset styles */
+@font-face {
+  font-family: 'fons';
+  src: url(data:font/woff2;base64,d09GMgABAAAAAADYAAoAAAAAAfgAAACRAAEAAAAAAAAAAAAAAAAAAAAAAAAAAAAABk4ALAocQAE2AiQDCAsGAAQgBQYHIBthAfgvCuwG7Rrd4iYMg5LqU0TZZLPfa3uCBGExyAo8AiFfOBzCEwgBJnLzILRYz/oGQLj+l531+0X9DxtwYJIwxhjgckLQwAFJASutQWvgVRhW3IeDQ9BALIKTcoFSguNfSAAMUAgKZ0ABUp+9f/b/hDYBBOFWP73XNPhevQ1ODZuA0GCCSQAAozAhGErgmwUA);
+}
+
 html {
   font-size: 1.25rem; /* 20px most likely */
+  font-family: 'fons';
 }
 
 body {
