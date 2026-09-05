e4bf22f7d211bcd1693fbbbd97afdf634cec8e33
John Doe <11800728+HotCherryPie@users.noreply.github.com>
2026-09-05 03:55:04 +0300

    wip

--- src/bits/switch-box/index.vue
+++ src/bits/switch-box/index.vue
@@ -6,7 +6,7 @@ interface Props {
 }
 
 withDefaults(defineProps<Props>(), {
-  color: '#32c257',
+  color: '#32C257',
 });
 
 const [model] = defineModel<boolean>({ default: false });
@@ -31,72 +31,64 @@ defineExpose({
     :aria-checked="model"
     @click="handleClick()"
   >
-    <div :class="$style.handle" />
+    <svg :class="$style.handle" viewBox="0 0 20 10">
+      <path
+        fill-rule="evenodd"
+        d="
+          M 0 0 H 20 V 10 H 0 Z
+          M 5 4
+          H 15
+          A 1 1 0 0 1 16 5
+          A 1 1 0 0 1 15 6
+          H 5
+          A 1 1 0 0 1 4 5
+          A 1 1 0 0 1 5 4
+          Z
+        "
+      />
+    </svg>
   </button>
 </template>
 
-<style lang="css" module>
+<style module>
 .it {
+  --transition: 90ms;
+  --backdrop: #0002; /* #8a8a8a54 */
+
   --size: 1.5rem;
   --radius: 0.45rem;
   --padding: calc(0.0625rem * 2);
-  --handle-size: calc(0.0625rem * 10);
-  --transition: 90ms;
 
-  /* Consider to natch only primary input method */
+  /* Consider to match only primary input method */
   @media (any-pointer: coarse) {
     --size: 2.5rem;
     --radius: 0.8rem;
     --padding: calc(0.0625rem * 4);
-    --handle-size: calc(0.0625rem * 16);
   }
-
-  width: var(--size);
-  aspect-ratio: 1;
-  border-radius: calc(var(--radius) - var(--padding));
-
-  background-color: #0002; /* #8a8a8a54 */
-
-  /* box-shadow:
-    inset 0 -1px 3px -1px #0003,
-    inset 0 1px 3px -1px #0003; */
-
-  /* background-image: linear-gradient(to bottom, #32c257, #32c257, #32c257); */
-  /* box-shadow: inset 0 0 3px 0px #00000069; */
 
   transition-property: background-color;
   transition-duration: var(--transition);
 
+  aspect-ratio: 1;
+  width: var(--size);
+  border-radius: calc(var(--radius) - var(--padding));
+  background-color: var(--backdrop);
   padding: var(--padding);
 }
 
 .handle {
+  /* 1/2 aspect ratio */
+  --handle-size: calc((var(--size) - (var(--padding) * 2)) / 2);
   --dy: calc((var(--size) / 2) - (var(--handle-size) / 2) - var(--padding));
-
-  height: var(--handle-size);
-  border-radius: calc(var(--radius) - var(--padding) * 2);
-  background-color: #fff; /* dark theme: #959595 */
-  box-shadow: 0 0.0625rem 0.0625rem 0 #0000001f;
 
   transition-property: translate;
   transition-duration: var(--transition);
 
+  border-radius: calc(var(--radius) - var(--padding) * 2);
+  fill: #fff; /* dark theme: #959595 */
   translate: 0 calc(-1 * var(--dy));
-
-  position: relative;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-
-  &::after {
-    content: '';
-    width: 0.75rem; /* 12px */
-    aspect-ratio: 12; /* 1px */
-    background: #ccc; /* dark theme: #4b4b4b */
-    position: absolute;
-    margin: auto;
-    border-radius: 999px;
-  }
+  box-shadow: 0 0.0625rem 0.0625rem 0 #0000001f;
+  /* filter: drop-shadow(0 0.0625rem 0.0625rem #0000001f); */
 }
 
 .it.on {
--- src/doodles/018-switch-box/index.vue
+++ src/doodles/018-switch-box/index.vue
@@ -21,11 +21,11 @@ const $i = useTemplateRef('i');
     <!-- linear-gradient(135deg in oklch, #fc466b, #3f5efb) -->
     <SwitchBox ref="a" color="#f5466f" data-silent @mouseenter="$a?.touch()" />
     <SwitchBox ref="b" color="#e13fa9" data-silent @mouseenter="$b?.touch()" />
-    <SwitchBox ref="c" color="#bc45d5" data-silent @mouseenter="$c?.touch()" />
-    <SwitchBox ref="d" color="#e23fa7" data-silent @mouseenter="$d?.touch()" />
+    <SwitchBox ref="c" color="#bf44d2" data-silent @mouseenter="$c?.touch()" />
+    <SwitchBox ref="d" color="#e13fa9" data-silent @mouseenter="$d?.touch()" />
     <SwitchBox ref="e" color="#bf44d2" data-silent @mouseenter="$e?.touch()" />
-    <SwitchBox ref="f" color="#8c4ff2" data-silent @mouseenter="$f?.touch()" />
-    <SwitchBox ref="j" color="#bd45d4" data-silent @mouseenter="$j?.touch()" />
+    <SwitchBox ref="f" color="#8f4ef1" data-silent @mouseenter="$f?.touch()" />
+    <SwitchBox ref="j" color="#bf44d2" data-silent @mouseenter="$j?.touch()" />
     <SwitchBox ref="h" color="#8f4ef1" data-silent @mouseenter="$h?.touch()" />
     <SwitchBox ref="i" color="#3f5dfb" data-silent @mouseenter="$i?.touch()" />
   </Bento.Cell>
