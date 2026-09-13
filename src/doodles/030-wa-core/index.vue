a7812e21f8dab6f5cb8cd2d71c7672093b122a4c
John Doe <11800728+HotCherryPie@users.noreply.github.com>
2026-09-13 12:01:28 +0300

    wip

--- src/doodles/030-wa-core/index.vue
+++ src/doodles/030-wa-core/index.vue
@@ -3,22 +3,42 @@ import { Bento } from '../../components';
 </script>
 
 <template>
-  <Bento.Cell w="2">
-    <div :class="$style.it">&lcub;<span>CORE</span>&rcub;</div>
+  <Bento.Cell w="2" h="2" subgrid>
+    <div :class="$style.it1" style="grid-area: 1 / 1 / 1 / -1">
+      &lcub;<span>CORE</span>&rcub;
+    </div>
+    <div :class="$style.it2" style="grid-area: 2 / 1 / 2 / -1">
+      &lsqb;<span>CORE</span>&rsqb;
+    </div>
   </Bento.Cell>
 </template>
 
 <style module>
-.it {
+.it1 {
+  text-box: trim-both cap alphabetic;
   font-family: 'Sono';
   font-weight: 500;
   font-size: 1.2rem;
   color: #0004;
-  /* color: rgb(from dodgerblue r g b / 0.3); */
 
   span {
     font-weight: 600;
     color: dodgerblue;
+  }
+}
+
+.it2 {
+  text-box: trim-both cap alphabetic;
+  font-family: 'Sono';
+  font-weight: 500;
+  font-size: 1.2rem;
+  color: #0004;
+
+  span {
+    font-weight: 600;
+    color: color(rec2020 0.94 0.83 0.05);
+    display: inline-block;
+    transform: translateY(0.05em);
   }
 }
 </style>
